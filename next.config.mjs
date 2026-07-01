ff15a95d780d9745924b0832b2dfccb12d87f661
Samuel Espìnpoza <samuel.espinoza@glr.pe>
2026-07-01 10:44:24 -0500

    solucion para vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,42 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         // Proxy para GraphQL
+//         source: '/api-proxy/graphql',
+//         destination: 'http://cronosprintedapi.glr.test/graphql',
+//       },
+//       {
+//         // Proxy para la API REST
+//         source: '/api-proxy/rest/:path*',
+//         destination: 'http://cronosprinted.glr.test/api/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         // Proxy para GraphQL
         source: '/api-proxy/graphql',
-        destination: 'http://cronosprintedapi.glr.test/graphql',
+        destination: isProd
+          ? (process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://ciao-approx-provinces-nickel.trycloudflare.com/graphql')
+          : 'http://cronosprintedapi.glr.test/graphql',
       },
       {
         // Proxy para la API REST
         source: '/api-proxy/rest/:path*',
-        destination: 'http://cronosprinted.glr.test/api/:path*',
+        destination: isProd
+          ? 'https://ciao-approx-provinces-nickel.trycloudflare.com/api/:path*'
+          : 'http://cronosprinted.glr.test/api/:path*',
       },
     ];
   },
